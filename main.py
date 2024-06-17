950f15c919c5025f2bba9bb12154381957123286
leonmnq <leon.mnq@gmail.com>
2024-06-17 14:41:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ rec = sr.Recognizer()
 with sr.Microphone(device_index=3) as microfone:
     rec.adjust_for_ambient_noise(microfone)
     print("Pode começar a falar:")
-
     # rec.pause_threshold = 1 # coloque isso se quiser que ele demore mais para entender que uma pausa é o fim do audio
-
     audio = rec.listen(microfone)
 try:
     # reconhece esse audio e traduz ele pra texto
     texto = rec.recognize_google(audio, language="pt-BR")
     print(texto)
 except:
-    print("Não peguei áudio nenhum")
+    # print("Não peguei áudio nenhum")
+    print("Não entendi o que você falou")
